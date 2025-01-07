0b2ea77e21ad73e738d393bd775d258913a98c43
YugeshVelkuru <yugeshbunny@gmail.com>
2025-01-07 14:59:13 +0530

    learnt base

---
+++ BasicRelations.java
@@ -0,0 +1,33 @@
+public class BasicRelations {
+    public static void main(String[] args) {
+
+        // lessthan -> '<'
+        int num1 = 9;
+        int num2 = 10;
+
+        boolean result = num1 < num2;
+
+        System.out.println("Result : " + result);
+        // greaterthan -> '>'
+        int a = 6;
+        int b = 2;
+
+        boolean ans = b > a;
+        System.out.println("Answer : " + ans);
+
+        //notequaltoo -> '!='
+
+        int h = 9;
+        int k = 9;
+
+        boolean verdict = h != k;
+        System.out.println("Verdict : " + verdict);
+
+        //equaltoo -> '=='
+
+        int i = 8;
+        int j = 0;
+        boolean judge = i == j;
+        System.out.println("Judge : " + judge);
+    }
+}
