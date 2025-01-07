1e5e63dbdcccd96ac375fcacae08309f77051c61
YugeshVelkuru <yugeshbunny@gmail.com>
2025-01-07 14:42:57 +0530

    learnt base

---
+++ BasicMath.java
@@ -0,0 +1,30 @@
+public class BasicMath {
+    public static void main(String[] args){
+        //add -> '+'
+        int a = 5;
+        int b = 9;
+
+        System.out.println(a+b);
+
+        //sub -> '-'
+
+        int num1 = 9;
+        int num2 = 7;
+        System.out.println(num1 - num2);
+
+        //div -> '/'
+        int i = 8;
+        int j = 4;
+        System.out.println(i/j);
+
+        //multi -> '*'
+        int c = 6;
+        int d = 9;
+        System.out.println(c * d);
+
+        //modulo -> '%'
+        int g = 15;
+        int k = 2;
+        System.out.println(k%g);
+    }
+}
