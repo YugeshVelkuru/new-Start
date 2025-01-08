8f5faecb8e1c6c4aa24cbde413e506b4dcc8325e
YugeshVelkuru <yugeshbunny@gmail.com>
2025-01-08 22:24:23 +0530

    learnt oops base

---
+++ OopsStart.java
@@ -0,0 +1,13 @@
+class Test{
+    public static void printTest (){
+        System.out.println("Hello world");
+    }
+}
+
+public class OopsStart {
+    public static void main(String[] args) {
+
+        Test.printTest();
+
+    }
+}
